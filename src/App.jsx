bdf1d48bc0956676bc45cc11f19bd5a1ef01e760
Drcodepro <drrahangdale01@gmail.com>
2025-02-03 00:11:29 +0530

    initial code added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import MenueBox from './component/MenueBox';
+
+function App() {
+
+  return (
+    <div id="AppContainer">
+      <MenueBox/>
+      <div></div>
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/MenueBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../All_css/MenueBox.css';
+
+const MenueBox = () => {
+  return (
+    <div id='menueContainer'>
+        mey name is raja
+    </div>
+  );
+};
+
+export default MenueBox;
